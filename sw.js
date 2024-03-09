6d598db0dcc05c605f587072d798e0208e825c92
rafiz001 <rafiz001@protonmail.ch>
2024-03-09 22:19:12 +0600

    PWA for caching done

--- sw.js
+++ sw.js
@@ -1,4 +1,28 @@
 //serviceWorker
-self.addEventListener('install', e=>{}); //install
+self.addEventListener('install', e=>{
+    e.waitUntil(
+        caches.open("Rafiz").then(cache => {
+          // Our application only has two files here index.html and manifest.json
+          // but you can add more such as style.css as your app grows
+          return cache.addAll([
+            './',
+            './index.html',
+            './manifest.json',
+            './s.png',
+            './c.png',
+          ]);
+        })
+      );
+}); //install
 self.addEventListener('activate', e=>{}); //activate
-self.addEventListener('fetch', e=>{}); //fetch
+self.addEventListener('fetch', e=>{
+
+    e.respondWith(
+        caches.open("Rafiz")
+          .then(cache => cache.match(e.request, { ignoreSearch: true }))
+          .then(response => {
+            return response || fetch(e.request);
+          })
+      );
+
+}); //fetch
